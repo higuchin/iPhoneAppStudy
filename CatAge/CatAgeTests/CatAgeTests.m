621746b2604ca2a531de4ee8de85b36f78985af5
Yasuyuki HIGUCHI <yh7180@gmail.com>
2014-03-02 23:30:09 +0900

    create project

---
+++ CatAge/CatAgeTests/CatAgeTests.m
@@ -0,0 +1,34 @@
+//
+//  CatAgeTests.m
+//  CatAgeTests
+//
+//  Created by yasuyuki on 2014/03/02.
+//  Copyright (c) 2014年 yasuyuki. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface CatAgeTests : XCTestCase
+
+@end
+
+@implementation CatAgeTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
