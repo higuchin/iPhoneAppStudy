d8ad857a918dfacd6ae173e8274ad1b8c783bea2
Yasuyuki HIGUCHI <yh7180@gmail.com>
2014-03-03 00:02:04 +0900

    第一段階実装

--- CatAge/CatAge/ViewController.m
+++ CatAge/CatAge/ViewController.m
@@ -10,6 +10,12 @@
 
 @interface ViewController ()
 
+@property (weak, nonatomic) IBOutlet UITextField *catField;
+@property (weak, nonatomic) IBOutlet UILabel *humanLabel;
+
+- (IBAction)calc:(UIButton *)sender;
+//- (double)catAgeToHumanAge:(double)catAge; // privateメソッドの宣言は省略可能
+
 @end
 
 @implementation ViewController
@@ -26,4 +32,25 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)calc:(UIButton *)sender {
+    double catAge = [_catField.text doubleValue];
+    double humanAge = [self catAgeToHumanAge:catAge];
+    _humanLabel.text = [NSString stringWithFormat:@"%.1f 歳", humanAge];
+}
+
+- (double)catAgeToHumanAge:(double)catAge {
+    double result = 0;
+    if (catAge <= 1) {
+        result = 20 * catAge;
+    } else if (catAge <= 5) {
+        result = catAge * 6 + 15;
+    } else if (catAge <= 10) {
+        result = catAge * 5 + 20;
+    } else if (catAge <= 15) {
+        result = catAge * 4 + 30;
+    } else {
+        result = catAge * 3 + 45;
+    }
+    return result;
+}
 @end
