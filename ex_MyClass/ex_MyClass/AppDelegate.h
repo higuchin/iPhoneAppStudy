a2e102babeb796bfe8aeadb1351bbdeb824c874c
Yasuyuki HIGUCHI <yh7180@gmail.com>
2014-03-04 09:24:00 +0900

    initial revision.

---
+++ ex_MyClass/ex_MyClass/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ex_MyClass
+//
+//  Created by yasuyuki on 2014/03/04.
+//  Copyright (c) 2014年 yasuyuki. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ex_MyClass/ex_MyClass/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ex_MyClass
+//
+//  Created by yasuyuki on 2014/03/04.
+//  Copyright (c) 2014年 yasuyuki. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
