8c8837d31403e21112aa3f91f87a537cdf4875aa
Yasuyuki HIGUCHI <yh7180@gmail.com>
2014-03-08 15:48:25 +0900

    initial revision.

---
+++ ImageSample/ImageSample/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ImageSample
+//
+//  Created by yasuyuki on 2014/03/08.
+//  Copyright (c) 2014年 yasuyuki. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
