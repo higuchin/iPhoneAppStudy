3dff4c5121453fe4960045ed5c7eb194b33cc54b
Yasuyuki HIGUCHI <yh7180@gmail.com>
2014-03-09 22:04:55 +0900

    add sample code.

--- ImageSample/ImageSample/ViewController.m
+++ ImageSample/ImageSample/ViewController.m
@@ -10,14 +10,28 @@
 
 @interface ViewController ()
 
+@property (weak, nonatomic) IBOutlet UIImageView *imageHolder;
+@property (weak, nonatomic) IBOutlet UIImageView *upperImage;
+- (IBAction)switchImage:(UISwitch *)sender;
+- (IBAction)upperImageSwitch:(UISwitch *)sender;
+
 @end
 
 @implementation ViewController
+{
+    UIImage *imageData;
+}
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+
+    imageData = [UIImage imageNamed:@"Blossom.jpg"];
+    _imageHolder.contentMode = UIViewContentModeScaleAspectFill;
+    _imageHolder.backgroundColor = [UIColor lightGrayColor];
+    _imageHolder.image = imageData;
+    _imageHolder.clipsToBounds = YES;
 }
 
 - (void)didReceiveMemoryWarning
@@ -26,4 +40,15 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)switchImage:(UISwitch *)sender {
+    if (sender.on == YES) {
+        _imageHolder.image = imageData;
+    } else {
+        _imageHolder.image = nil;
+    }
+}
+
+- (IBAction)upperImageSwitch:(UISwitch *)sender {
+    _upperImage.hidden = !sender.on;
+}
 @end
