ee74df91f92f6ee5e77474dbb2ce91bcfaae47e7
Yasuyuki HIGUCHI <yh7180@gmail.com>
2014-03-09 22:43:04 +0900

    initial revision.

---
+++ CreateButtonSample/CreateButtonSample/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CreateButtonSample
+//
+//  Created by yasuyuki on 2014/03/09.
+//  Copyright (c) 2014年 yasuyuki. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
