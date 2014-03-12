479fd7a7c81ab7e2fc48df39ed1328b3ea098ea5
Yasuyuki HIGUCHI <yh7180@gmail.com>
2014-03-13 00:15:22 +0900

    initial revision.

---
+++ TouchEventSample/TouchEventSample/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TouchEventSample
+//
+//  Created by yasuyuki on 2014/03/13.
+//  Copyright (c) 2014年 yasuyuki. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TouchEventSample/TouchEventSample/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  TouchEventSample
+//
+//  Created by yasuyuki on 2014/03/13.
+//  Copyright (c) 2014年 yasuyuki. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
