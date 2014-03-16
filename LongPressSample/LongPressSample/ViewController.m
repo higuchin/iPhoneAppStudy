18b6065fc152c32de0a9e63fe50d4d52df308b4c
Yasuyuki HIGUCHI <yh7180@gmail.com>
2014-03-16 23:18:52 +0900

    implement longPress

--- LongPressSample/LongPressSample/ViewController.m
+++ LongPressSample/LongPressSample/ViewController.m
@@ -9,6 +9,8 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIImageView *flower;
+- (IBAction)longPress:(UILongPressGestureRecognizer *)sender;
 
 @end
 
@@ -18,6 +20,8 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    _flower.hidden = YES;
 }
 
 - (void)didReceiveMemoryWarning
@@ -26,4 +30,18 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)longPress:(UILongPressGestureRecognizer *)sender {
+    if (sender.state == UIGestureRecognizerStateBegan) {
+        CGPoint point = [sender locationInView:self.view];
+        _flower.center = CGPointMake(point.x, -50);
+        _flower.hidden = NO;
+        CGFloat height = _flower.bounds.size.height;
+        [UIView animateWithDuration:0.5 delay:0.0
+                            options:UIViewAnimationOptionCurveEaseOut
+                         animations:^{
+                             _flower.center = CGPointMake(point.x, point.y - (height/2));
+                         }
+                         completion:nil];
+    }
+}
 @end
