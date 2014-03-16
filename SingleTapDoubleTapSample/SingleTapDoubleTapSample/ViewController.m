e4102e7ea5a333c5b4728960727475d0ba7024bc
Yasuyuki HIGUCHI <yh7180@gmail.com>
2014-03-17 00:10:24 +0900

    implement tap event

--- SingleTapDoubleTapSample/SingleTapDoubleTapSample/ViewController.m
+++ SingleTapDoubleTapSample/SingleTapDoubleTapSample/ViewController.m
@@ -10,6 +10,8 @@
 
 @interface ViewController ()
 
+- (void) singleTap:(UITapGestureRecognizer *)gesture;
+- (void) doubleTap:(UITapGestureRecognizer *)gesture;
 @end
 
 @implementation ViewController
@@ -18,12 +20,39 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    // シングルタップ
+    UITapGestureRecognizer *singleTapGesture = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(singleTap:)];
+    singleTapGesture.numberOfTapsRequired = 1;
+    [self.view addGestureRecognizer:singleTapGesture];
+    
+    // ダブルタップ
+    UITapGestureRecognizer *doubleTapGesture = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(doubleTap:)];
+    doubleTapGesture.numberOfTapsRequired = 2;
+    [self.view addGestureRecognizer:doubleTapGesture];
+
+    // シングルタップの設定
+    [singleTapGesture requireGestureRecognizerToFail:doubleTapGesture];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void) singleTap:(UITapGestureRecognizer *)gesture
+{
+    self.view.backgroundColor = [UIColor whiteColor];
+}
+
+- (void) doubleTap:(UITapGestureRecognizer *)gesture
+{
+    if (self.view.backgroundColor == [UIColor redColor]) {
+        self.view.backgroundColor = [UIColor blueColor];
+    } else {
+        self.view.backgroundColor = [UIColor redColor];
+    }
 }
 
 @end
