1856f8e890c0e8aa4ffb7aaf2cd9e926b08a56ad
Yasuyuki HIGUCHI <yh7180@gmail.com>
2014-03-16 23:50:23 +0900

    implement swipe

--- SwipeSample/SwipeSample/ViewController.m
+++ SwipeSample/SwipeSample/ViewController.m
@@ -10,6 +10,12 @@
 
 @interface ViewController ()
 
+@property (weak, nonatomic) IBOutlet UIView *frontView;
+- (IBAction)swipeView:(UISwipeGestureRecognizer *)sender;
+
+@property (weak, nonatomic) IBOutlet UIButton *coverButton;
+- (IBAction)closeFrontView:(UIButton *)sender;
+
 @end
 
 @implementation ViewController
@@ -24,6 +30,29 @@
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+    
+    _coverButton.hidden = YES;
 }
 
+- (IBAction)swipeView:(UISwipeGestureRecognizer *)sender {
+    CGPoint location = _frontView.center;
+    CGFloat center_x = self.view.center.x;
+    if (sender.direction == UISwipeGestureRecognizerDirectionRight) {
+        location.x = center_x + 120;
+        _coverButton.hidden = NO;
+    } else if (sender.direction == UISwipeGestureRecognizerDirectionLeft) {
+        location.x = center_x;
+        _coverButton.hidden = YES;
+    }
+    
+    [UIView animateWithDuration:0.5 animations:^{
+        _frontView.center = location;
+    }];
+}
+- (IBAction)closeFrontView:(UIButton *)sender {
+    _coverButton.hidden = YES;
+    [UIView animateWithDuration:0.5 animations:^{
+        _frontView.center = self.view.center;
+    }];
+}
 @end
