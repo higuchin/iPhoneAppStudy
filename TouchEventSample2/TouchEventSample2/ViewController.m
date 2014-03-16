73796b1e659cb18553d5d11680c77782981704b8
Yasuyuki HIGUCHI <yh7180@gmail.com>
2014-03-16 21:46:45 +0900

    create project

---
+++ TouchEventSample2/TouchEventSample2/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  TouchEventSample2
+//
+//  Created by yasuyuki on 2014/03/16.
+//  Copyright (c) 2014年 yasuyuki. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ TouchEventSample2/TouchEventSample2Tests/TouchEventSample2Tests.m
@@ -0,0 +1,34 @@
+//
+//  TouchEventSample2Tests.m
+//  TouchEventSample2Tests
+//
+//  Created by yasuyuki on 2014/03/16.
+//  Copyright (c) 2014年 yasuyuki. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface TouchEventSample2Tests : XCTestCase
+
+@end
+
+@implementation TouchEventSample2Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
