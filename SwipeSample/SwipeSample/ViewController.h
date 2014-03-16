51b7ec8e32386811e6b29460323052e689091197
Yasuyuki HIGUCHI <yh7180@gmail.com>
2014-03-16 23:21:00 +0900

    create project

---
+++ SwipeSample/SwipeSample/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SwipeSample
+//
+//  Created by yasuyuki on 2014/03/16.
+//  Copyright (c) 2014年 yasuyuki. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
