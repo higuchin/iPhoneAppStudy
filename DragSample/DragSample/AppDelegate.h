a9fa348f223b3d9573aafb7bffc6764cc57d3c36
Yasuyuki HIGUCHI <yh7180@gmail.com>
2014-03-16 22:18:19 +0900

    create project

---
+++ DragSample/DragSample/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  DragSample
+//
+//  Created by yasuyuki on 2014/03/16.
+//  Copyright (c) 2014年 yasuyuki. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
