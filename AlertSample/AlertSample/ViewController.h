1233c81cbd37a6a8a079c43d8f8153272e82f5cc
Yasuyuki HIGUCHI <yh7180@gmail.com>
2014-03-18 00:51:36 +0900

    create project.

---
+++ AlertSample/AlertSample/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  AlertSample
+//
+//  Created by yasuyuki on 2014/03/18.
+//  Copyright (c) 2014年 yasuyuki. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
