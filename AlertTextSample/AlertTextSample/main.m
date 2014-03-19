fb9242248270e4d9cec54774149927edf012c760
Yasuyuki HIGUCHI <yh7180@gmail.com>
2014-03-19 23:31:26 +0900

    プロジェクト作成

---
+++ AlertTextSample/AlertTextSample/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  AlertTextSample
+//
+//  Created by yasuyuki on 2014/03/19.
+//  Copyright (c) 2014年 yasuyuki. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
