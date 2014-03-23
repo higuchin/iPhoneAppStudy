21e5ab0bf424abd05667d3aa0a84a88a1becd1b1
Yasuyuki HIGUCHI <yh7180@gmail.com>
2014-03-23 21:48:08 +0900

    プロジェクト作成

---
+++ TextDelegateSample/TextDelegateSample/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  TextDelegateSample
+//
+//  Created by yasuyuki on 2014/03/23.
+//  Copyright (c) 2014年 yasuyuki. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
