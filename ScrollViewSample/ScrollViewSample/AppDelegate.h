3b54e3d839167cf1259e590e90e0615b5880bfee
Yasuyuki HIGUCHI <yh7180@gmail.com>
2014-03-23 22:02:55 +0900

    プロジェクト作成

---
+++ ScrollViewSample/ScrollViewSample/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ScrollViewSample
+//
+//  Created by yasuyuki on 2014/03/23.
+//  Copyright (c) 2014年 yasuyuki. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
