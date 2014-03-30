1e0748ccb9bf5ec5249e7e76663957afcbe92f1e
Yasuyuki HIGUCHI <yh7180@gmail.com>
2014-03-30 14:27:00 +0900

    プロジェクト作成

---
+++ ScrollViewPagingSample/ScrollViewPagingSample/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ScrollViewPagingSample
+//
+//  Created by yasuyuki on 2014/03/30.
+//  Copyright (c) 2014年 yasuyuki. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ ScrollViewPagingSample/ScrollViewPagingSampleTests/ScrollViewPagingSampleTests.m
@@ -0,0 +1,34 @@
+//
+//  ScrollViewPagingSampleTests.m
+//  ScrollViewPagingSampleTests
+//
+//  Created by yasuyuki on 2014/03/30.
+//  Copyright (c) 2014年 yasuyuki. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ScrollViewPagingSampleTests : XCTestCase
+
+@end
+
+@implementation ScrollViewPagingSampleTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
