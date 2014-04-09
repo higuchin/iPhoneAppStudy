26034db7af3d21b05e8c5d982a2449a8094bb0c9
Yasuyuki HIGUCHI <yh7180@gmail.com>
2014-04-09 23:57:23 +0900

    プロジェクト作成

---
+++ MasterDetailSample/MasterDetailSample/DetailViewController.h
@@ -0,0 +1,16 @@
+//
+//  DetailViewController.h
+//  MasterDetailSample
+//
+//  Created by yasuyuki on 2014/04/09.
+//  Copyright (c) 2014年 yasuyuki. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+
+@property (strong, nonatomic) id detailItem;
+
+@property (weak, nonatomic) IBOutlet UILabel *detailDescriptionLabel;
+@end
---
+++ MasterDetailSample/MasterDetailSample/MasterViewController.h
@@ -0,0 +1,13 @@
+//
+//  MasterViewController.h
+//  MasterDetailSample
+//
+//  Created by yasuyuki on 2014/04/09.
+//  Copyright (c) 2014年 yasuyuki. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MasterViewController : UITableViewController
+
+@end
