9868fb033d280a2fd00a7ac6ca8fe546c2f18b5a
Yasuyuki HIGUCHI <yh7180@gmail.com>
2014-04-12 23:36:52 +0900

    サンプル作成

--- MasterDetailSample/MasterDetailSample/MasterViewController.m
+++ MasterDetailSample/MasterDetailSample/MasterViewController.m
@@ -11,7 +11,7 @@
 #import "DetailViewController.h"
 
 @interface MasterViewController () {
-    NSMutableArray *_objects;
+    NSArray *tableDataList;
 }
 @end
 
@@ -25,11 +25,16 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
-    self.navigationItem.leftBarButtonItem = self.editButtonItem;
-
-    UIBarButtonItem *addButton = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemAdd target:self action:@selector(insertNewObject:)];
-    self.navigationItem.rightBarButtonItem = addButton;
+    
+    NSDictionary *cell0 = @{@"site":@"Apple", @"url":@"http://www.apple.com/jp/"};
+    NSDictionary *cell1 = @{@"site":@"Apple Store", @"url":@"http://store.apple.com/jp"};
+    NSDictionary *cell2 = @{@"site":@"Yahoo!ニュース", @"url":@"http://news.yahoo.co.jp/"};
+    NSDictionary *cell3 = @{@"site":@"Google ニュース", @"url":@"https://news.google.co.jp/"};
+    
+    NSDictionary *section1 = @{@"header":@"Apple", @"data":@[cell0, cell1]};
+    NSDictionary *section2 = @{@"header":@"ニュース", @"data":@[cell2, cell3]};
+    
+    tableDataList = @[section1, section2];
 }
 
 - (void)didReceiveMemoryWarning
@@ -38,76 +43,55 @@
     // Dispose of any resources that can be recreated.
 }
 
-- (void)insertNewObject:(id)sender
-{
-    if (!_objects) {
-        _objects = [[NSMutableArray alloc] init];
-    }
-    [_objects insertObject:[NSDate date] atIndex:0];
-    NSIndexPath *indexPath = [NSIndexPath indexPathForRow:0 inSection:0];
-    [self.tableView insertRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationAutomatic];
-}
-
 #pragma mark - Table View
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
 {
-    return 1;
+    return tableDataList.count;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-    return _objects.count;
+    NSDictionary *sectionData = tableDataList[section];
+    NSArray *data = sectionData[@"data"];
+    return data.count;
+}
+
+- (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section
+{
+    NSDictionary *sectionData = tableDataList[section];
+    return sectionData[@"header"];
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"Cell" forIndexPath:indexPath];
 
-    NSDate *object = _objects[indexPath.row];
-    cell.textLabel.text = [object description];
+    NSDictionary *cellData = [self cellDataAtIndexPath:indexPath];
+
+    cell.textLabel.text = cellData[@"site"];
+    cell.detailTextLabel.text = cellData[@"url"];
+    
     return cell;
 }
-
-- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    // Return NO if you do not want the specified item to be editable.
-    return YES;
-}
-
-- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    if (editingStyle == UITableViewCellEditingStyleDelete) {
-        [_objects removeObjectAtIndex:indexPath.row];
-        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
-    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
-        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view.
-    }
-}
-
-/*
-// Override to support rearranging the table view.
-- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath
-{
-}
-*/
-
-/*
-// Override to support conditional rearranging of the table view.
-- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    // Return NO if you do not want the item to be re-orderable.
-    return YES;
-}
-*/
 
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
 {
     if ([[segue identifier] isEqualToString:@"showDetail"]) {
+        
         NSIndexPath *indexPath = [self.tableView indexPathForSelectedRow];
-        NSDate *object = _objects[indexPath.row];
-        [[segue destinationViewController] setDetailItem:object];
+        NSDictionary *cellData = [self cellDataAtIndexPath:indexPath];
+        NSString *url = cellData[@"url"];
+        [[segue destinationViewController] setDetailItem:url];
     }
+}
+
+-(NSDictionary *)cellDataAtIndexPath:(NSIndexPath *)indexPath
+{
+    NSDictionary *sectionData = tableDataList[indexPath.section];
+    NSArray *cellArray = sectionData[@"data"];
+    NSDictionary *cellData = cellArray[indexPath.row];
+    return cellData;
 }
 
 @end
